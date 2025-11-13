287313fda5357459e39e6f0b57f9a27c2dd35689
torquuato <158479344+torquuato@users.noreply.github.com>
2025-11-13 20:56:23 +0000

    Delete treasurehunt directory

--- treasurehunt/mobile.js
+++
@@ -1,7 +0,0 @@
-    function checkScreenSize() {
-      const isMobile = window.innerWidth < 768;
-      document.getElementById("mobileWarning").style.display = isMobile ? "flex" : "none";
-      document.getElementById("desktopContent").style.display = isMobile ? "none" : "block";
-    }
-    window.addEventListener("load", checkScreenSize);
-    window.addEventListener("resize", checkScreenSize);
